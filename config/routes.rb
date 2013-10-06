96c0a7885a3232d3ba6be61c281cfbdb5ab29a89
Ahmed Nadar <ahmedfay@hotmail.com>
2013-10-06 01:23:10 -0400

    Add products controller with Index, Show, New and Edit actions, along with the views. Plus update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Merchant::Application.routes.draw do
+  get "products/index"
+  get "products/show"
+  get "products/new"
+  get "products/edit"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
