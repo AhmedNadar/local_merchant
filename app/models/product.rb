e47a5c833c381bfa2612c50d0615f80b7df8d928
Ahmed Nadar <ahmedfay@hotmail.com>
2013-10-07 14:25:33 -0400

    Override ActiveRecord method to handle delete! $ from price Getter

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+	validates_numericality_of :price
+
+	# override ActiveRecord method to handel removing $ from price at the Getter.
+	def price=(input)
+		input.delete!("$")
+		super
+	end
 end
