31a72fca9180e350e07d2d5980d84336a37c0e76
Ahmed Nadar <ahmedfay@hotmail.com>
2013-10-08 10:11:30 -0400

    validates stock

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,6 +7,7 @@ class Product < ActiveRecord::Base
 	end
 
 	#validations
-	validates_presence_of :title, :description, :image_url, :price 
+	validates_presence_of :title, :description, :image_url, :price, :stock
 	validates_numericality_of :price
+	validates_numericality_of :stock, :greater_than_or_equal_to => 0
 end
