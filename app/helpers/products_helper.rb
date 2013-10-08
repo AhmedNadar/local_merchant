bf6fce8608e1178a7cd4377dba2dc525bb6ffdaa
Ahmed Nadar <ahmedfay@hotmail.com>
2013-10-08 10:05:58 -0400

    fix 'display_stock' method

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,5 +1,16 @@
 module ProductsHelper
+	
 	def print_price(price)
 		number_to_currency price
+	end
+
+	def display_stock(stock)
+		if stock > 0
+			content_tag :span, "In Stock" + stock.to_s
+			# <span>In Stock (##)</span>
+		else
+			content_tag :span, "Out of Stock"
+			# <span>Out of Stock</span>
+		end
 	end
 end
