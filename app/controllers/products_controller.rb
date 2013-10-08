5294cd0542fc0d88b3e0fb22c5ac2a64d4a29492
Ahmed Nadar <ahmedfay@hotmail.com>
2013-10-08 10:10:57 -0400

    add stock to stromng params

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -62,6 +62,6 @@ class ProductsController < ApplicationController
   # Never trust parameters from the scary internet, only allow the white list through.
   # This tells Rails to allow all four of our Product attributes to be mass-assign-able.
   def product_params
-  	params.require(:product).permit(:title, :description, :price, :image_url )
+  	params.require(:product).permit(:title, :description, :price, :image_url, :stock )
   end
 end
