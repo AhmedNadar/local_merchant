5f8536429eeafe35b0f1d753ac41abc95aa86d71
Ahmed Nadar <ahmedfay@hotmail.com>
2013-10-08 12:57:29 -0400

    update title validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ActiveRecord::Base
 
+  default_scope :order => 'title'
+
 	# override ActiveRecord method to handel removing $ from price at the Getter.
 	def price=(input)
 		input.delete!("$")
@@ -8,6 +10,7 @@ class Product < ActiveRecord::Base
 
 	#validations order is ipmortant for view 
   validates_uniqueness_of :title
+  validates_length_of :title, :minimum => 13
 	validates_presence_of :title, :description, :image_url, :price, :stock
 	validates_numericality_of :price, :greater_than_or_equal_to => 0.01
 	validates_numericality_of :stock, :greater_than_or_equal_to => 0
