a7f37d4f9de525f78e24f758ca2526a3d4cc40b4
Ahmed Nadar <ahmedfay@hotmail.com>
2013-10-08 13:49:31 -0400

    store cart to session by indexing it with :cart_id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+  # Rails makes the current session look like a hash to the controller, 
+  # so we’ll store the cart in the session by indexing it with the symbol :cart_id.
+  def current_cart 
+    Cart.find(session[:cart_id])
+  rescue ActiveRecord::RecordNotFound
+    cart = Cart.create
+    session[:cart_id] = cart.id
+    cart
+  end
 end
