782ba1484ca273e282cbbd6426defb40caee0bc7
Ahmed Nadar <ahmedfay@hotmail.com>
2013-10-08 16:22:41 -0400

    add associations to items

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,18 +1,32 @@
 class Product < ActiveRecord::Base
 
-  default_scope :order => 'title'
-
-	# override ActiveRecord method to handel removing $ from price at the Getter.
+ 	# override ActiveRecord method to handel removing $ from price at the Getter.
 	def price=(input)
 		input.delete!("$")
 		super
 	end
+  
+  # order by title
+  default_scope :order => 'title'
+  has_many :line_items
+  
+  # ensure that there are no line items referencing this product before deletng product
+  before_destroy :ensure_not_referenced_by_any_line_item    
+  # a hook method
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add :base, "Line Items present"
+      return false
+    end
+  end
 
 	#validations order is ipmortant for view 
   validates_uniqueness_of :title
-  validates_length_of :title, :minimum => 13
+  validates_length_of :title, :minimum => 5
 	validates_presence_of :title, :description, :image_url, :price, :stock
 	validates_numericality_of :price, :greater_than_or_equal_to => 0.01
 	validates_numericality_of :stock, :greater_than_or_equal_to => 0
-  validates_format_of :image_url, :with => %r{\.(gif|jpg|png)z}i
+  validates_format_of :image_url, :with => /\.(gif|jpg|png)\z/i
 end
