8759f7e5fd102d5c573e29e8f6e64cf664340ac5
Ahmed Nadar <ahmedfay@hotmail.com>
2013-10-07 21:02:11 -0400

    Add validation for all attrebuites. Order is important, it reflects on the view

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,12 @@
 class Product < ActiveRecord::Base
-	validates_numericality_of :price
 
 	# override ActiveRecord method to handel removing $ from price at the Getter.
 	def price=(input)
 		input.delete!("$")
 		super
 	end
+
+	#validations
+	validates_presence_of :title, :description, :image_url, :price 
+	validates_numericality_of :price
 end
