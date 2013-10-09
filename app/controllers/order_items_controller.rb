496c43b252f334c4b16bf790f6acf1ddf047f8b7
Ahmed Nadar <ahmedfay@hotmail.com>
2013-10-09 17:49:59 -0400

    update creat action

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -3,29 +3,34 @@ class OrderItemsController < ApplicationController
   before_action :set_item, only: [:show, :new, :update, :destroy]
   before_action :load_order, only: [:create]
 
-  def index
-    @order_items = OrderItem.all
-  end
+  # def index
+  #   @order_items = OrderItem.all
+  # end
 
-  def new
-    @order_item = OrderItem.new
-  end
+  # def new
+  #   @order_item = OrderItem.new
+  # end
 
   def edit
   end
 
-  def show
-  end
+  # def show
+  # end
 
-  def create
+  def create    
     # @order_item = OrderItem.new(order_item_params)
-    @order_item = OrderItem.new(product_id: params[:product_id], order_id: @order.id)
+    @order_item = @order.order_items.new(quantity:1, product_id: params[:product_id])
+    # @order_item = OrderItem.new(product_id: params[:product_id], order_id: @order.id)
 
-    if @order_item.save
-      #redirect_to @order_item, notice: 'Item saved successfully.' }
-      redirect_to @order, notice: 'Yeaay, successfully added product to cart.' 
-    else
-       render :new
+    respond_to do |format|
+      if @order_item.save
+        #redirect_to @order_item, notice: 'Item saved successfully.' }
+        format.html { redirect_to @order, notice: 'Yeaay, successfully added product to cart.' }
+        format.json { render action: 'show', status: :created, location: @order_item }
+      else
+         format.html { render action 'new' }
+         format.json { render json: @order_item.errors, status: unprocessable_entity }
+      end
     end
   end
 
@@ -43,12 +48,12 @@ class OrderItemsController < ApplicationController
   end
 
   private
+
   # before we add new item
   def load_order
-    begin
-      @order = Order.find(session[:order_id])               
-    rescue ActiveRecord::RecordNotFound
-      @order = Order.create(status: "not submitted yet!")
+    @order = Order.find_or_initialize_by_id(session[:order_id], status: "not submitted yet!")               
+    if @order.new_record?
+      @order.save!
       session[:order_id] = @order_id
     end
   end
