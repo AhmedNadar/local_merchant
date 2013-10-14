7c0beff3774ae362c16d35d80b7d9d6c2ca6fd6a
Ralph Bolo <ralph.bolo@gmail.com>
2013-10-14 16:40:13 -0400

    order confirmation mailer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131014054308) do
+ActiveRecord::Schema.define(version: 20131014201913) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "addresses", force: true do |t|
     t.string   "line1"
@@ -37,16 +40,6 @@ ActiveRecord::Schema.define(version: 20131014054308) do
     t.datetime "updated_at"
     t.integer  "order_id"
     t.integer  "quantity",   default: 0
-  end
-
-  create_table "order_payments", force: true do |t|
-    t.string   "email"
-    t.decimal  "subtotal",   precision: 8, scale: 2, default: 0.0
-    t.decimal  "tax",        precision: 8, scale: 2, default: 0.0
-    t.decimal  "total",      precision: 8, scale: 2, default: 0.0
-    t.integer  "order_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "orders", force: true do |t|
